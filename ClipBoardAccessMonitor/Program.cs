a8c0aaefb6aee3cff6199ed9cfa501202a3503ee
Suminda De Silva <sumindaniro@gmail.com>
2021-06-24 08:40:20 +0530

    Initial commit

---
+++ ClipBoardAccessMonitor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClipBoardAccessMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process currentProcess = null;
+            while (true)
+            {
+                int processId;
+                User32.GetWindowThreadProcessId((int)User32.GetClipboardOwner(), out processId);
+
+
+                if (Process.GetProcessById(processId).Id != (currentProcess != null ? currentProcess.Id : -1))
+                {
+                    currentProcess = Process.GetProcessById(processId);
+                    Console.WriteLine(currentProcess.ProcessName);
+                }
+            }
+        }
+    }
+}
---
+++ ClipBoardAccessMonitor/User32.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClipBoardAccessMonitor
+{
+    public class User32
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        public const int SHOW_MAXIMIZED = 3;
+        public const int SW_SHOW = 5;
+        public const int SW_SHOWNORMAL = 1;
+        public const int SW_RESTORE = 9;
+        public const int SW_MINIMIZE = 6;
+
+        public const UInt32 WM_GETTEXT = 0x000D;
+        public const UInt32 WM_GETTEXTLENGTH = 0x000E;
+        public const UInt32 WM_KEYDOWN = 0x0100;
+        public const UInt32 WM_KEYUP = 0x0101;
+        public const UInt32 WM_SYSCOMMAND = 0x0112;
+
+        public const int VK_INSERT = 0x2D;
+        public const int VK_CONTROL = 0x11;
+        public const int VK_KEY_A = 0x41;
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern short VkKeyScan(char ch);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetOpenClipboardWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool BringWindowToTop(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32")]
+        public static extern UInt32 GetWindowThreadProcessId(Int32 hWnd, out Int32 lpdwProcessId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        public static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int extraInfo);
+
+        [DllImport("user32.dll")]
+        public static extern short MapVirtualKey(int wCode, int wMapType);
+
+        [DllImport("user32.dll")]
+        public static extern short SetActiveWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern int AttachThreadInput(int CurrentForegroundThread, int MakeThisThreadForegrouond, bool boolAttach);
+
+        [DllImport("user32")]
+        public static extern IntPtr GetClipboardOwner();
+        //[DllImport("user32.dll", CharSet = CharSet.Auto)]
+        //public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, StringBuilder lParam);
+
+    }
+}
